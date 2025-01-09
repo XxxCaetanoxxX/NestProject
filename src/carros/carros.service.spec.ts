b0cce66ddc4544f0fc48fd863c2b9e81838b53b7
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2025-01-09 08:24:52 -0300

    fix: adicionando findAllCarsDto no arquivo de teste

--- src/carros/carros.service.spec.ts
+++ src/carros/carros.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CarrosService } from './carros.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateCarroDto } from './dto/create-carro.dto';
+import { FindAlLCarsDto } from './dto/find-all-cars.dto';
 
 
 describe('CarrosService', () => {
@@ -9,6 +10,10 @@ describe('CarrosService', () => {
   let prisma: PrismaService;
   let carro: CreateCarroDto;
   let insertedCar: any;
+  const findAllCarsFto: FindAlLCarsDto = {
+    limit: 10,
+    offset: 0
+  }
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,17 +32,17 @@ describe('CarrosService', () => {
   });
 
   it('deve retronar uma lista de carros', async () => {
-    const result = await service.findAll();
+    const result = await service.findAll(findAllCarsFto);
     expect(result).toBeDefined();
   });
 
   it('deve retornar mais de 2 carros', async () => {
-    const result = await service.findAll();
+    const result = await service.findAll(findAllCarsFto);
     expect(result.length).toBeGreaterThan(6);
   });
 
   it('deve ter um "dodge" como primeiro carro', async () => {
-    const result = await service.findAll();
+    const result = await service.findAll(findAllCarsFto);
     expect(result[0].nome).toBe('dodge');
   });
 
