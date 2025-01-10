6b3c7058b7e4cc9a432b460dcbb532c9a00bf3af
Caetano César <caetanocesar35@gmail.com>
2025-01-10 18:40:50 -0300

    fix: adicionando apiproperty no endpoint de login

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,12 +1,15 @@
 import { IsNotEmpty, IsString, MinLength, Matches, IsEnum } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class SignInDto {
+    @ApiProperty()
     @IsString({ message: "name must be string" })
     @MinLength(3, { message: "name must contain at least 3 characters" })
     @IsNotEmpty({ message: "name can't be empty" })
     name: string;
 
+    @ApiProperty()
     @IsString({ message: "password must be string" })
     @MinLength(4, { message: "password must contain at least 4 characters" })
     @Matches(/.*\d.*/, { message: "password must contain at least 1 number" }) //estudar essa linha
